19a4fc4dba344f4e28b7d554705fdba25feb2215
aomine <aomine1745@gmail.com>
2021-02-11 12:49:23 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const ztxt = new Ztextify(".text-layers", {
+  depth: "50px",
+  layers: 80,
+  fde: true,
+  direction: "forwards",
+  eventDirection: "reverse",
+  event: "pointer",
+  eventRotation: "35deg",
+});
+
+const zemoji = new Ztextify(".emoji-layers", {
+  depth: "50px",
+  layers: 80,
+  fde: true,
+  direction: "forwards",
+  event: "pointer",
+  eventRotation: "35deg",
+});
